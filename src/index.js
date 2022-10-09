4b979ed9795a2b204769d8264e94e7c7c8ed1e26
Anirudh A V <anirudh.av02@gmail.com>
2022-10-10 01:06:58 +0530

    Syncfusion lisence key registered

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { registerLicense } from '@syncfusion/ej2-base';
 
 import "./index.css";
 import App from "./App";
+
+// Registering Syncfusion license key
+registerLicense(process.env.REACT_APP_SYNCFUSION_LICENSE_KEY);
 
 ReactDOM.render(<App />, document.getElementById("root"));

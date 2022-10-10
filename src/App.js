8f5821673e011f681591b40652a98f60e3d482ed
Anirudh A V <anirudh.av02@gmail.com>
2022-10-10 18:30:17 +0530

    Line component imported

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from "./pages";
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor } from "./pages";
 import "./App.css";
 
 const App = () => {

1efc7225dbfb3faf0524d23688a42e8c101a2ad7
Anirudh A V <anirudh.av02@gmail.com>
2022-10-10 19:13:57 +0530

    ContextProvider added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { registerLicense } from '@syncfusion/ej2-base';
 
 import "./index.css";
 import App from "./App";
+import { ContextProvider } from "./contexts/ContextProvider";
 
 // Registering Syncfusion license key
 registerLicense(process.env.REACT_APP_SYNCFUSION_LICENSE_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+    <ContextProvider>
     <App />
+    </ContextProvider>
 );

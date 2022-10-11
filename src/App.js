8c6604955cfb84ac5749c4d7198b42a1917d5a57
Anirudh A V <anirudh.av02@gmail.com>
2022-10-11 08:09:17 +0530

    Context API configured

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor } from "./pages";
+
+import { useStateContext } from "./contexts/ContextProvider";
+
 import "./App.css";
 
 const App = () => {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
+
+    // const activeMenu = true;
 
     return (
         <div>

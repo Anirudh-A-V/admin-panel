b12d693b0f4bc9454672b1d8f2de0d430abb99b0
Anirudh A V <anirudh.av02@gmail.com>
2022-10-13 23:55:52 +0530

    Navbar completed > Components not yet created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,8 @@ const Navbutton = ({ title, Func, icon, color, dotColor }) => (
       style={{ color }}
       className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
       <span style={{ background: dotColor }}
-        className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'>
+        className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'/>
         {icon}
-      </span>
     </button>
   </TooltipComponent>
 )

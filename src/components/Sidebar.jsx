b347239fddeeb1927e71bc413d90252d24c641f5
Anirudh A V <anirudh.av02@gmail.com>
2022-10-13 23:56:21 +0530

    Sidebar will close onclick in smaller devices

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,13 +8,18 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
   
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
+  const hancleCloseSidebar = () => {
+    if( activeMenu && screenSize <= 900 ) {
+      setActiveMenu(false);
+    }
+  }
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -22,7 +27,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={hancleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware />{" "}
@@ -55,7 +60,7 @@ const Sidebar = () => {
                       <NavLink
                         to={`/${link.name}`}
                         key={link.name}
-                        onClick={() => { }}
+                        onClick={hancleCloseSidebar}
                         className={({ isActive }) => isActive ? activeLink : normalLink}
                       >
                         {link.icon}

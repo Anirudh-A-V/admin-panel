132d91fc9af6829e3713a89ade33dffb09bf0d69
Anirudh A V <anirudh.av02@gmail.com>
2022-10-14 00:02:45 +0530

    Ecommerce.jsx all imports

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { BiRupee } from "react-icons/bi";
+import { GoPrimitiveDot } from "react-icons/go";
+
+import { Stacked, Pie, Button, Sparkline } from "../components";
+import { earningData, SparklineAreaData } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Ecommerce = () => {
   return (

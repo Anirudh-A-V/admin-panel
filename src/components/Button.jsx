2979145df3fae854cdc6e7c9fcc5dd01749a3c94
Anirudh A V <anirudh.av02@gmail.com>
2022-10-14 00:41:11 +0530

    Button Component configured

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
   return (
-    <div>Button</div>
+    <button className={`text-${size} p-3 hover:drop-shadow-xl`}
+      style={{ backgroundColor: bgColor, color: color, borderRadius: borderRadius }}
+    >
+      {text}
+    </button>
   )
 }
 

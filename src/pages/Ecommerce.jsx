5ba32a982b718996d12b4293c4f6237a1cb23a8f
Anirudh A V <anirudh.av02@gmail.com>
2022-10-15 10:40:47 +0530

    Stacked chart added > Margin given to the contents of revenue section > SparkLine chart bugs fixed

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -57,7 +57,7 @@ const Ecommerce = () => {
       <div className='flex gap-10 flex-wrap justify-center'>
         <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
           <div className='flex justify-between'>
-            <p className='font-semibold text-xl'>Revenue Updates</p>
+            <p className='font-semibold text-2xl'>Revenue Updates</p>
             <div className='flex gap-4 items-center'>
               <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
                 <span><GoPrimitiveDot /></span>
@@ -71,7 +71,7 @@ const Ecommerce = () => {
           </div>
           <div className='mt-10 flex gap-10 flex-wrap justify-center'>
             <div className='border-r-1 border-color m-4 pr-10'>
-              <div>
+              <div >
                 <p>
                   <span className='text-3xl font-semibold'>₹23,45,234</span>
                   <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>18%</span>
@@ -80,11 +80,8 @@ const Ecommerce = () => {
                   Budget
                 </p>
               </div>
-              <div>
-                <p>
-                  <span className='text-3xl font-semibold'>₹17,44,234</span>
-
-                </p>
+              <div className="mt-8">
+                  <p className='text-3xl font-semibold'>₹17,44,234</p>
                 <p className='text-gray-500'>
                   Expense
                 </p>
@@ -101,7 +98,20 @@ const Ecommerce = () => {
                   color='blue'
                 />
               </div>
-              
+              <div className='mt-10'>
+                <Button 
+                  color='white'
+                  bgColor='blue'
+                  text='Download Report'
+                  borderRadius='10px'
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked 
+                width="320px"
+                height="360px"
+              />
             </div>
           </div>
         </div>

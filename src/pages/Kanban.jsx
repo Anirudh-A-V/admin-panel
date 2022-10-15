39cd9e93931c78513d7b8609b7fa6ff63e0f35e4
Anirudh A V <anirudh.av02@gmail.com>
2022-10-15 23:49:08 +0530

    Kanban Page configured

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
+
+import { kanbanData, kanbanGrid } from '../data/dummy'
+import { Header } from '../components'
 
 const Kanban = () => {
   return (
-    <div>Kanban</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header title='Kanban' category="App" />
+      <KanbanComponent
+        id="kanban"
+        dataSource={kanbanData}
+        keyField="Status"
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) =>
+            <ColumnDirective key={index} headerText={item.headerText} keyField={item.keyField} allowToggle={item.allowToggle} isExpanded={item.isExpanded} />
+          )}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
   )
 }
 

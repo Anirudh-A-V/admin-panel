3d3ce3852c2cf8361fa257eb7d2b22a3495edf05
Anirudh A V <anirudh.av02@gmail.com>
2022-10-20 22:59:12 +0530

    Theme Options partially configured

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsCheck } from "react-icons/bs";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { themeColors } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const ThemeSettings = () => {
-  return (
-    <div>ThemeSettings</div>
-  )
-}
 
-export default ThemeSettings
+  const { activeMenu, setActiveMenu, clicked, setClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+
+  return (
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-80">
+        <div className="flex justify-between items-center p-4 ml-4">
+          <p className="font-semibold text-lg">Settings</p>
+          <button
+            type="button"
+            onClick={() => { }}
+            style={{
+              color: "rgb(153, 171, 180)",
+              borderRadius: "50%",
+            }}
+            className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
+          >
+            <MdOutlineCancel />
+          </button>
+        </div>
+
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-lg">Theme Options</p>
+
+          <div className="mt-4">
+            <input
+              type='radio'
+              id='light'
+              name='theme'
+              value='light'
+              className='cursor-pointer'
+              onChange={() => { }}
+              checked={true}
+            />
+            <label htmlFor="light" className="ml-2 text-md cursor-pointer">
+              Light
+            </label>
+          </div>
+          <div className="mt-4">
+            <input
+              type='radio'
+              id='dark'
+              name='theme'
+              value='Dark'
+              className='cursor-pointer'
+              onChange={() => { }}
+              checked={true}
+            />
+            <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
+              Dark
+            </label>
+          </div>
+        </div>
+
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-lg">Theme Colors</p>
+
+          <div className="flex gap-3">
+            {themeColors.map((item, index) => (
+              <TooltipComponent key={index} content={item.name} position='TopCenter'>
+                <div className="relative mt-2 cursor-pointer flex gap-5 items-center">
+                  <button
+                  type="button"
+                    className='h-10 w-10 rounded-full cursor-pointer'
+                    style={{}}
+                  >
+                    <BsCheck className={`ml-2 text-2xl text-white ${true ? 'block' : 'hidden'}`} />
+                  </button>
+                </div>
+              </TooltipComponent>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThemeSettings;

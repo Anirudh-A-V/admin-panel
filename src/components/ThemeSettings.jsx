db5a59180e6b4b25489c19e93e7c720989bc86db
Anirudh A V <anirudh.av02@gmail.com>
2022-10-22 17:15:04 +0530

    Theme settings dark mode fixed

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -12,7 +12,7 @@ const ThemeSettings = () => {
 
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
-      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-80">
+      <div className="float-right h-screen dark:text-gray-200 bg-white dark:bg-[#484B52] w-80">
         <div className="flex justify-between items-center p-4 ml-4">
           <p className="font-semibold text-lg">Settings</p>
           <button

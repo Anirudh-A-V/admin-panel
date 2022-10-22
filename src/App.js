5a0ad021f230094bcbbd05d5017041a477728eb0
Anirudh A V <anirudh.av02@gmail.com>
2022-10-22 16:23:29 +0530

    Sidebar width reduced

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
                         </TooltipComponent>
                     </div>
                     {activeMenu ? (
-                        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+                        <div className="w-64 fixed sidebar dark:bg-secondary-dark-bg bg-white">
                             <Sidebar />
                         </div>
                     ) : (

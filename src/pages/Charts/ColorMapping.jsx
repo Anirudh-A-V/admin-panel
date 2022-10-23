442c1252dffc1e1fe468e18dd42cd0864c363d30
Anirudh A V <anirudh.av02@gmail.com>
2022-10-24 01:27:05 +0530

    ColorMapping bar graph configured

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, ColumnSeries, Category, Tooltip, Legend, RangeColorSettingsDirective, RangeColorSettingDirective } from '@syncfusion/ej2-react-charts';
+
+import { useStateContext } from '../../contexts/ContextProvider'
+import { ChartsHeader } from '../../components';
+import { colorMappingData, rangeColorMapping, ColorMappingPrimaryXAxis, ColorMappingPrimaryYAxis } from '../../data/dummy';
 
 const ColorMapping = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>ColorMapping</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white  dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartsHeader category='Color Mapping' title='USA CLIMATE - WEATHER BY MONTH' />
+      <div className='w-full'>
+        <ChartComponent
+          id="charts"
+          primaryXAxis={ColorMappingPrimaryXAxis}
+          primaryYAxis={ColorMappingPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          legendSettings={{ mode: 'Range', background: '#FFFFFF' }}
+          
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject services={[ColumnSeries, Category, Tooltip, Legend]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={colorMappingData[0]}
+              xName='x'
+              yName='y'
+              name='USA'
+              type='Column'
+              cornerRadius={{
+                topLeft: 10,
+                topRight: 10,
+              }}
+            />
+          </SeriesCollectionDirective>
+          <RangeColorSettingsDirective>
+            {rangeColorMapping.map((item, index) =>
+              <RangeColorSettingDirective
+                key={index}
+                start={item.start}
+                end={item.end}
+                colors={item.colors}
+              />
+            )}
+          </RangeColorSettingsDirective>
+        </ChartComponent>
+      </div>
+    </div>
   )
 }
 

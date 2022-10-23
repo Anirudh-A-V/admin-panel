2903eaa94374a7b8e0d0ea8b54852d6aca203d82
Anirudh A V <anirudh.av02@gmail.com>
2022-10-24 00:28:43 +0530

    Financial Chart fixed
    Dark mode now supported
    Filtering data as per the available data
    Removed Chart Border
    Added Tooltip
    Added Crosshair
    Added Zooming
    Added Pan
    Renamed the title
    Changed the color of the candle sticks

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -3,36 +3,54 @@ import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Hil
 
 import { ChartsHeader } from '../../components'
 import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const date1 = new Date('2017, 1, 1');
+
+function filterValue(value) {
+  if (value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
+}
+const returnValue = financialChartData.filter(filterValue);
 
 const Financial = () => {
+  const { currentMode } = useStateContext();
+
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white  dark:bg-secondary-dark-bg rounded-3xl'>
-      <ChartsHeader category='Candle Stick' title='Project Cost Breakdown' />
+      <ChartsHeader category='Candle Stick' title='Apple Stocks' />
       <div className='w-full'>
         <ChartComponent
           id='charts'
           style={{ textAlign: 'center' }}
           primaryXAxis={FinancialPrimaryXAxis}
           primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: 'Vertical', line: { width: 0 } }}
+          background={currentMode === 'Dark' ? '#33373E' : '#FFFFFF'}
+          zoomSettings={{ enableSelectionZooming: true, mode: 'X', enablePan: true, enableMouseWheelZooming: true }}
           legendSettings={{ visible: false }}
         >
           <Inject services={[CandleSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom]} />
           <SeriesCollectionDirective>
             <SeriesDirective
-              dataSource={financialChartData}
+              dataSource={returnValue}
               xName='x'
               yName='low'
-              name='SHIRPUR-G'
+              name='Apple Inc'
               type='Candle'
               low='low'
               high='high'
               open='open'
               close='close'
-              bearFillColor='#e56590'
-              bullFillColor='#f8b883'
+              enableSolidCandles={true}
+              bearFillColor='#00ff2a'
+              bullFillColor='#ff0000'
             />
           </SeriesCollectionDirective>
-        </ChartComponent>;
+        </ChartComponent>
       </div>
     </div>
   )

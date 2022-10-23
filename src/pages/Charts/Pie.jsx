1ba3b2baa603fa7458e08ba73c17063c4f212989
Anirudh A V <anirudh.av02@gmail.com>
2022-10-24 00:00:24 +0530

    Pie chart page configured

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import {Pie as PieChart, ChartsHeader } from '../../components'
+import { pieChartData } from '../../data/dummy'
+
 const Pie = () => {
   return (
-    <div>
-      Pie
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white  dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartsHeader category='Pie' title='Project Cost Breakdown' />
+      <div className='w-full'>
+        <PieChart id="chart-pie" data={pieChartData} legendVisiblity height="full"/>
+      </div>
     </div>
   )
 }

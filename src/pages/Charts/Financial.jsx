b5c76b1dd8a6de20334512fd25a7370f2b08377f
Anirudh A V <anirudh.av02@gmail.com>
2022-10-24 00:17:33 +0530

    Financial chart implemented

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, HiloSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom, CandleSeries } from '@syncfusion/ej2-react-charts';
+
+import { ChartsHeader } from '../../components'
+import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../../data/dummy'
 
 const Financial = () => {
   return (
-    <div>
-      Financial
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white  dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartsHeader category='Candle Stick' title='Project Cost Breakdown' />
+      <div className='w-full'>
+        <ChartComponent
+          id='charts'
+          style={{ textAlign: 'center' }}
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          legendSettings={{ visible: false }}
+        >
+          <Inject services={[CandleSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={financialChartData}
+              xName='x'
+              yName='low'
+              name='SHIRPUR-G'
+              type='Candle'
+              low='low'
+              high='high'
+              open='open'
+              close='close'
+              bearFillColor='#e56590'
+              bullFillColor='#f8b883'
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>;
+      </div>
     </div>
   )
 }

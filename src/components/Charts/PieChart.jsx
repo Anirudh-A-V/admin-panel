e82dd31fb66c01c39ac1c11c02ce7dac8d99d485
Anirudh A V <anirudh.av02@gmail.com>
2022-10-24 00:00:12 +0530

    PieChart component configured

--- src/components/Charts/PieChart.jsx
+++ src/components/Charts/PieChart.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, Inject, AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip } from '@syncfusion/ej2-react-charts';
 
-const PieChart = () => {
+
+import { useStateContext } from '../../contexts/ContextProvider'
+
+const PieChart = ({ id, data, legendVisiblity, height }) => {
+  const { currentMode } = useStateContext();
   return (
-    <div>PieChart</div>
+    <AccumulationChartComponent
+      id={id}
+      height={height}
+      background={currentMode === 'Dark' ? '#33373E' : '#FFFFFF'}
+      legendSettings={{ visible: legendVisiblity }}
+      tooltip={{ enable: true }}
+      enableSmartLabels={true}
+    >
+      <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          name="Sale"
+          dataSource={data}
+          xName="x"
+          yName="y"
+          innerRadius="40%"
+          startAngle={0}
+          endAngle={360}
+          radius="70%"
+          explode
+          explodeOffset="10%"
+          explodeIndex={2}
+          dataLabel={{
+            visible: true,
+            name: 'text',
+            position: 'Inside',
+            font: {
+              fontWeight: '600',
+              color: '#fff',
+            },
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
   )
 }
 

781768200c38c23108d932f3243042f2f610e70c
Anirudh A V <anirudh.av02@gmail.com>
2022-10-28 12:11:48 +0530

    Userprofile component completed

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -8,7 +8,7 @@ import { Button } from '.'
 import avatar from '../data/avatar.jpg'
 
 const UserProfile = () => {
-  const { activeMenu, setActiveMenu, clicked, setScreenSize, handleClick, screenSize, currentColor } = useStateContext();
+  const { currentColor, initialState, setClicked } = useStateContext();
   return (
     <div className='nav-item absolute right-1 top16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96'>
       <div className='flex justify-between items-center'>
@@ -21,7 +21,51 @@ const UserProfile = () => {
           bg-HoverColor='light-gray'
           size='2xl'
           borderRadius='50%'
-          // Func={() => handleClick('userProfile')}
+          Func={() => setClicked(initialState)}
+        />
+      </div>
+      <div className='flex gap-5 items-center mt-6 border-color border-b-1 pb-6'>
+        <img
+          className='rounded-full w-20 h-20'
+          src={avatar}
+          alt='avatar'
+        />
+        <div>
+          <p className='font-semibold text-xl dark:text-gray-200'>Michael Roberts</p>
+          <p className='text-sm text-gray-400 dark:text-gray-400'>
+            Administrator</p>
+          <p className='text-sm font-semibold text-gray-400 dark:text-gray-400'>
+            info@shop.com</p>
+        </div>
+      </div>
+      <div>
+        {userProfileData.map((item, index) => (
+          <div key={index} className='flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer dark:hover:bg-[#42464D]'>
+            <button
+              type='button'
+              className='text-xl rounded-lg p-3 hover:bg-light-gray'
+              style={{ backgroundColor: item.iconBg, color: item.iconColor }}
+            >
+              {item.icon}
+            </button>
+            <div>
+              <p className='font-semibold dark:text-gray-200'>
+                {item.title}
+              </p>
+              <p className='text-sm text-gray-500 dark:text-gray-400'>
+                {item.desc}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='mt-5'>
+        <Button
+          text='Logout'
+          color='white'
+          bgColor={currentColor}
+          borderRadius='10px'
+          width='full'
         />
       </div>
     </div>

3adbe43a1c5c784f96780b221f8196bf1bd9beca
Anirudh A V <anirudh.av02@gmail.com>
2022-10-28 13:36:43 +0530

    Footer component added to App.js

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ const App = () => {
                                 <Route path="/stacked" element={<Stacked />} />
                             </Routes>
                         </div>
+                        <Footer />
                     </div>
                 </div>
             </BrowserRouter>

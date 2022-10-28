37eca770efe1794e5fe806561cee9fe7e5b09fca
Anirudh A V <anirudh.av02@gmail.com>
2022-10-28 12:10:58 +0530

    Button Component updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Button = ({ bgColor, color, size, text, borderRadius }) => {
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width, Func }) => {
   return (
-    <button className={`text-${size} p-3 hover:drop-shadow-xl`}
-      style={{ backgroundColor: bgColor, color: color, borderRadius: borderRadius }}
+    <button
+      type="button"
+      onClick={Func}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
     >
-      {text}
+      {icon} {text}
     </button>
   )
 }
